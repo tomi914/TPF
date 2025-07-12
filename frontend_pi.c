f7bd154b88cce92d10b63dd639048cbb594b2f07
tomi914 <tgarcilazo@itba.edu.ar>
2025-07-11 23:27:48 -0300

    Update frontend_pi.c

--- frontend_pi.c
+++ frontend_pi.c
@@ -15,26 +15,92 @@
 #include "backend.h"
 
 //PROTOTIPOS FUNCIONES
-int getDir(joyinfo_t* joy); //si se mueve a la izq o a la der
+int getDirX(joyinfo_t* joy); //si se mueve a la izq o a la der
+int getDirY(joyinfo_t* joy, clock_t* last); //si se mueve hacia arriba o hacia abajo (para elegir opciones en el menu)
 bool getShoot(joyinfo_t* joy); //disparo
 bool getPause(joyinfo_t* joy, clock_t* last); //pausa
+void drawPlay(void);
+void drawRestart(void);
+void drawExit(void);
 
 //VARIABLES GLOBALES
 player_t player;
 bullet_t pBullet;
 alienBlock_t block;
 alien_t alien [5][5];
+stats_t stats;
+
+int playSymbol[16][16] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+int restartSymbol[16][16] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
+    {0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0},
+    {0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+int exitSymbol[16][16] = {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0},
+    {0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0},
+    {0,0,0,0,1,1,1,0,0,1,1,1,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0},
+    {0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0},
+    {0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0},
+    {0,0,0,0,1,1,1,0,0,1,1,1,0,0,0,0},
+    {0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0},
+    {0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
 
 int main(void){
 	joyinfo_t info;
 	joy_init();
-    clock_t clk = clock();
+    clock_t clkP = clock(); //clock pause
+    clock_t clkS = clock(); //clock select
+    //clock_t clkA; //clock aliens moving
     int i, j;
     bool shoot;
+    //int lastRowToPrint = -1;
     
     initPlayer(&player);
     initAliensBlock(&block);
     initAliensArray(alien);
+    initGameStats(&stats);
     
 
 	dcoord_t myPoint;
@@ -43,26 +109,55 @@ int main(void){
 
     while(1){
         disp_clear();
-        if(getPause(&info, &clk)){
-            while(!getPause(&info, &clk)){
-                myPoint.x = 15;
-                myPoint.y = 15;
-                disp_write(myPoint, D_ON);
+        if(getPause(&info, &clkP)){
+            clkS = clock();
+            while(!getPause(&info, &clkP)){
+                drawExit();
                 disp_update();
+                printf("%d\n", getDirY(&info, &clkS));
             }
         }
-        /*updateAliensBlock(&alien, &block);
-        blockMove(alien, &block);*/
+
+        //updateAliensBlock(alien, &block);
+        /*if(lastRowToPrint == 4){
+            blockMove(alien, &block);
+        }
+        for(i=4; i>=0; i--){
+            for(j=0; j<5; j++){
+                if(alien[i][j].alive){
+                    if(lastRowToPrint==-1){
+                        myPoint.x = alien[i][j].coord.coordX;
+                        myPoint.y = alien[i][j].coord.coordY;
+                        disp_write(myPoint, D_ON);
+                        lastRowToPrint = 4;
+                        clkA = clock();
+                    }
+                    if(lastRowToPrint==i && ((double)(clock()-clkA)/CLOCKS_PER_SEC)>=0.5){
+                        clkA = clock();
+                        myPoint.x = alien[i][j].coord.coordX + block.coord.coordX;
+                        myPoint.y = alien[i][j].coord.coordY + block.coord.coordY;
+                        disp_write(myPoint, D_ON);
+                        lastRowToPrint--;
+                    } else{
+                        myPoint.x = alien[i][j].coord.coordX + block.coord.coordX - 1 * block.direction;
+                        myPoint.y = alien[i][j].coord.coordY + block.coord.coordY - 1 * block.direction;
+                        disp_write(myPoint, D_ON);
+                    }
+                }
+            }
+        }*/
+
+        blockMove(alien, &block);
         for(i=0; i<5; i++){
             for(j=0; j<5; j++){
                 if(alien[i][j].alive){
-                    myPoint.x = alien[i][j].coord.coordX;
-                    myPoint.y = alien[i][j].coord.coordY;
+                    myPoint.x = alien[i][j].coord.coordX + block.coord.coordX;
+                    myPoint.y = alien[i][j].coord.coordY + block.coord.coordY;
                     disp_write(myPoint, D_ON);
                 }
             }
         }
-        playerMove(getDir(&info), &player);
+        playerMove(getDirX(&info), &player);
         for(i=0; i<3; i++){
             myPoint.x = player.coord.coordX + i;
             myPoint.y = player.coord.coordY;
@@ -73,6 +168,7 @@ int main(void){
         if(pBullet.active){
             myPoint.x = pBullet.coord.coordX;
             myPoint.y = pBullet.coord.coordY;
+            collisionBA(&pBullet, alien, &block, &stats, 0);
         }
         disp_write(myPoint, D_ON);
         disp_update();
@@ -81,7 +177,7 @@ int main(void){
 	return 0;
 }
 
-int getDir(joyinfo_t* joy){ //Determina la direccion del player
+int getDirX(joyinfo_t* joy){ //Determina la direccion del player
 	*joy = joy_read();
 	if(joy->x>10){ //Retorna 1 si se mueve a la derecha
 		return 1;
@@ -89,6 +185,27 @@ int getDir(joyinfo_t* joy){ //Determina la direccion del player
 	if(joy->x<-10){ //Retorna -1 si se mueve a la izquierda
 		return -1;
 	}
+	return 0;
+}
+
+int getDirY(joyinfo_t* joy, clock_t* last){
+    static int state = 0;
+    *joy = joy_read(); 
+	if(joy->y>10){ //Retorna 1 si se mueve hacia arriba
+        if(((double)(clock()-*last)/CLOCKS_PER_SEC)<=0.2 && !state){
+            *last = clock();
+            state = 1;
+            return 1;
+        }
+	}else if(joy->y<-10){ //Retorna -1 si se mueve hacia abajo
+		if(((double)(clock()-*last)/CLOCKS_PER_SEC)<=0.2 && !state){
+            *last = clock();
+            state = 1;
+            return -1;
+        }
+	} else{
+        state = 0;
+    }
 	return 0;
 }
 
@@ -124,7 +241,7 @@ bool getPause(joyinfo_t* joy, clock_t* last){
         click++;
         usleep(10000);
     }
-    if((((double)(clock()-*last)/CLOCKS_PER_SEC)>=0.5 && click) || click == 2){
+    if((((double)(clock()-*last)/CLOCKS_PER_SEC)>=0.2 && click) || click == 2){
         pressing = 0;
         if(click == 2){
             click = 0;
@@ -136,51 +253,45 @@ bool getPause(joyinfo_t* joy, clock_t* last){
     return false;
 }
 
-/*int getPress(joyinfo_t* joy, clock_t* last){ //cuenta la cantidad de clicks en un intervalo de tiempo a partir de un click
-    static int pressing = 0;
-    static int click = 0;
-    *joy = joy_read();
-    if(joy->sw == J_PRESS && !click && !pressing){
-        pressing = 1;
-        *last = clock();
-        usleep(10000);
-    }
-    if(!pressing && joy->sw == J_PRESS){
-        pressing = 1;
-        usleep(10000);
-    }
-    if(pressing && joy->sw == J_NOPRESS){
-        pressing = 0;
-        click++;
-        usleep(10000);
-    }
-    if(((double)(clock()-*last)/CLOCKS_PER_SEC)>=0.3 && click){
-        int state = click;
-        click = 0;
-        pressing = 0;
-        return state;
-    }
-    return 0;
-}*/
-
-    /*if(joy->sw==J_PRESS || pressed){ //funcion que si mantengo apretado mas de x tiempo devuelve otro numero
-        pressed=1;
-        if(((double)(clock()-*last)/CLOCKS_PER_SEC)>=1){
-            if(joy->sw==J_NOPRESS){
-
+void drawPlay(void){
+    int i, j;
+    dcoord_t point;
+    for(i=0; i<16; i++){
+        for(j=0; j<16; j++){
+            if(playSymbol[i][j]){
+                point.x = j;
+                point.y = i;
+                disp_write(point, D_ON);
             }
-            
-            pressed=0;
-            return -1; //pausa
-        } 
-        if(joy->sw==J_PRESS){
-            pressed=0;
-            return 1; //bala
         }
-        return 20;
-	}else{
-        pressed=0;
-        *last=clock();
-        return 0;
-    }*/
+    }
+}
+
+void drawRestart(void){
+    int i, j;
+    dcoord_t point;
+    for(i=0; i<16; i++){
+        for(j=0; j<16; j++){
+            if(restartSymbol[i][j]){
+                point.x = j;
+                point.y = i;
+                disp_write(point, D_ON);
+            }
+        }
+    }
+}
+
+void drawExit(void){
+    int i, j;
+    dcoord_t point;
+    for(i=0; i<16; i++){
+        for(j=0; j<16; j++){
+            if(exitSymbol[i][j]){
+                point.x = j;
+                point.y = i;
+                disp_write(point, D_ON);
+            }
+        }
+    }
+}
     
